5635391de5f8b1a6d7ade649c2ae5b9cfb44b3b9
Rafal Jankowski <rafal.jankowski@neptune.ai>
2021-06-07 10:41:30 +0200

    Fastai minimal version requirement

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def main():
         'pylintfileheader'
     ]
 
-    base_libs = ['neptune-client>=0.9.8', 'fastai']
+    base_libs = ['neptune-client>=0.9.8', 'fastai>=2.0.0']
 
     version = None
     if os.path.exists('PKG-INFO'):
