5da81c74d816645bbb2c028c0b9ddd342cf609c6
Rafal Jankowski <rafal.jankowski@neptune.ai>
2021-06-14 19:34:19 +0200

    Minor fix

--- examples/vision_classification.py
+++ examples/vision_classification.py
@@ -1,11 +1,25 @@
-from neptune import new as neptune
-from neptune_fastai.impl import NeptuneCallback
-
+#
+# Copyright (c) 2021, Neptune Labs Sp. z o.o.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 from itertools import islice
 
 from fastai.basics import untar_data, URLs, error_rate
 from fastai.vision.all import ImageDataLoaders, get_image_files, Resize, cnn_learner, resnet34
-from fastai.callback.all import SaveModelCallback
+
+from neptune import new as neptune
+from neptune_fastai.impl import NeptuneCallback
 
 
 def is_cat(x):
@@ -13,7 +27,7 @@ def is_cat(x):
 
 
 def main():
-    neptune_run = neptune.init(run='FAS-200')
+    neptune_run = neptune.init()
 
     path = untar_data(URLs.PETS) / 'images'
 
@@ -28,15 +42,12 @@ def main():
     learn = cnn_learner(dls,
                         resnet34,
                         metrics=error_rate,
-                        cbs=[
-                            #NeptuneCallback(neptune_run, 'experiment', save_best_model=False),
-                            # SaveModelCallback()
-                        ],
+                        cbs=[NeptuneCallback(neptune_run, 'experiment')],
                         pretrained=False)
 
-    learn.fit_one_cycle(1, cbs=[NeptuneCallback(neptune_run, 'experiment', save_best_model=False)])
+    learn.fit_one_cycle(1)
     learn.fine_tune(2)
-    learn.fit(2, cbs=[NeptuneCallback(neptune_run, 'experiment', save_best_model=False)])
+    learn.fit(2)
 
 
 if __name__ == '__main__':
