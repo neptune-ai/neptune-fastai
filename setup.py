02caa74c01fdde0d98403ec2c0b430d45a6db543
Jakub Kuszneruk <jakub.kuszneruk@neptune.ai>
2021-06-17 18:10:45 +0200

    Fix runtime version

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 import os
 
-from setuptools import setup
+from setuptools import find_packages, setup
 
-from neptune_fastai._version import get_versions
+import versioneer
 
 
 def main():
@@ -35,7 +35,7 @@ def main():
             if line.startswith('Version:'):
                 version = line[8:].strip()
     else:
-        version = get_versions()["version"]
+        version = versioneer.get_version()
 
     setup(
         name='neptune-fastai',
@@ -56,7 +56,8 @@ def main():
         license='Apache License 2.0',
         install_requires=base_libs,
         extras_require=extras,
-        packages=['neptune_fastai', 'neptune_fastai.impl'],
+        packages=find_packages(),
+        cmdclass=versioneer.get_cmdclass(),
         zip_safe=False,
         classifiers=[
             # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
