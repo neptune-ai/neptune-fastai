036a765876620b9842a46159f232a5e9c46bcd49
Rafał Jankowski <rafal.jankowski@neptune.ai>
2021-06-17 15:52:20 +0200

    TrackerCallback and saving checkpoints internally (#13)
    * Saving model checkpoints internally
    
    * Samples restored
    
    * Cleanup on demand
    
    * Warning added

--- examples/tabular_model.py
+++ examples/tabular_model.py
@@ -15,7 +15,6 @@
 #
 from fastai.basics import URLs, untar_data, accuracy
 from fastai.tabular.all import tabular_learner, TabularDataLoaders, Categorify, FillMissing, Normalize
-from fastai.callback.all import SaveModelCallback
 
 from neptune import new as neptune
 from neptune_fastai.impl import NeptuneCallback
@@ -40,16 +39,10 @@ def main():
                                       cont_names=['age', 'fnlwgt', 'education-num'],
                                       procs=[Categorify, FillMissing, Normalize])
 
-    learn = tabular_learner(dls,
-                            metrics=accuracy)
-    learn.fit_one_cycle(10,
-                        cbs=[
-                                NeptuneCallback(run=neptune_run,
-                                                base_namespace='experiment',
-                                                save_best_model=True,
-                                                save_model_freq=4),
-                                SaveModelCallback(monitor='accuracy', every_epoch=True)
-                            ])
+    learn = tabular_learner(dls, metrics=accuracy)
+    learn.fit_one_cycle(10, cbs=[NeptuneCallback(run=neptune_run,
+                                                 base_namespace='experiment',
+                                                 save_model_freq=4)])
 
 
 if __name__ == '__main__':
--- tests/neptune_fastai/test_base.py
+++ tests/neptune_fastai/test_base.py
@@ -15,7 +15,6 @@
 #
 from fastai.basics import accuracy
 from fastai.tabular.all import tabular_learner
-from fastai.callback.tracker import SaveModelCallback
 
 from neptune_fastai.impl import NeptuneCallback
 
@@ -184,15 +183,13 @@ class TestBase:
 
     def test_saving_from_method(self, run, dataset):
         learn = tabular_learner(dataset, metrics=accuracy, layers=[10, 10])
-        learn.fit_one_cycle(1, cbs=[SaveModelCallback(), NeptuneCallback(run=run, save_best_model=True)])
+        learn.fit_one_cycle(1, cbs=[NeptuneCallback(run=run, save_best_model=True)])
 
         learn = tabular_learner(dataset, metrics=accuracy, layers=[10, 10])
-        learn.fit_one_cycle(2, cbs=[SaveModelCallback(every_epoch=True),
-                                    NeptuneCallback(run=run, save_best_model=False, save_model_freq=2)])
+        learn.fit_one_cycle(2, cbs=[NeptuneCallback(run=run, save_best_model=False, save_model_freq=2)])
 
         learn = tabular_learner(dataset, metrics=accuracy, layers=[10, 10])
-        learn.fit_one_cycle(2, cbs=[SaveModelCallback(every_epoch=True),
-                                    NeptuneCallback(run=run, save_best_model=True, save_model_freq=2)])
+        learn.fit_one_cycle(2, cbs=[NeptuneCallback(run=run, save_best_model=True, save_model_freq=2)])
 
         run.sync()
 
